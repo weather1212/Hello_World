98d8e0cfdc2e25a00a71008178f2dd755097463b
weather1212 <60419960+weather1212@users.noreply.github.com>
2020-02-05 15:57:56 +0900

    print hello

---
+++ src/hello_word/Hello_word.java
@@ -0,0 +1,12 @@
+package hello_word;
+
+public class Hello_word {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Çï·Î¿ì ¿ùµå!");
+
+	}
+
+}
